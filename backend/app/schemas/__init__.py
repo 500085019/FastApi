25328ddd742971ebe2ddb2889bc5b91d10d841b0
500085019 <prasadnaman387@gmail.com>
2026-04-16 01:16:05 +0530

    more backend code

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,7 @@
+from .user import UserCreate, UserOut, UserUpdate
+from .item import ItemCreate, ItemOut, ItemUpdate
+
+__all__ = [
+    "UserCreate", "UserOut", "UserUpdate",
+    "ItemCreate", "ItemOut", "ItemUpdate"
+]
